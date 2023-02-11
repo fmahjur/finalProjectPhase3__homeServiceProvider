0bd41b786adda30ad885b849a7e7979dbb29f81e
reihaneh <reihaneh763@gmail.com>
2023-02-12 01:12:12 +0330

    add ChangePasswordDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ChangePasswordDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ChangePasswordDTO.java
@@ -1,2 +1,14 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;public class ChangePasswordDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChangePasswordDTO {
+    String username;
+    String password;
 }
