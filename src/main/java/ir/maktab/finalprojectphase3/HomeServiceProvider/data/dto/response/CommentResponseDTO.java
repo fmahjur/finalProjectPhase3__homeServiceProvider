20a337ebb6567247c3aac6b3705b10a8a3c8e7a1
reihaneh <reihaneh763@gmail.com>
2023-02-11 23:55:25 +0330

    add CommentResponseDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/CommentResponseDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/CommentResponseDTO.java
@@ -1,2 +1,14 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;public class CommentResponseDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CommentResponseDTO {
+    String comment;
+    double score;
 }
