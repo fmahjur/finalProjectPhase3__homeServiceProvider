b71fe3fb8813c7fb65092ea3e460cf3ad46fb3cd
reihaneh <reihaneh763@gmail.com>
2023-02-12 01:15:59 +0330

    add ExpertResponseDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/ExpertResponseDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/ExpertResponseDTO.java
@@ -1,2 +1,18 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;public interface ExpertResponseDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.enums.ExpertStatus;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Credit;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExpertResponseDTO {
+    String firstname;
+    String lastname;
+    String emailAddress;
+    ExpertStatus expertStatus;
 }
