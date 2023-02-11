507090899adfe393ff5b8efc9051aca9dec644df
reihaneh <reihaneh763@gmail.com>
2023-02-12 01:14:29 +0330

    add ExpertGetCommentDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ExpertGetCommentDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ExpertGetCommentDTO.java
@@ -1,17 +1,20 @@
 package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;
 
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.enums.ExpertStatus;
 import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Comment;
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.SubService;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExpertRequestDTO {
-    ExpertStatus expertStatus;
-    byte[] personalPhoto;
-    List<SubService> subServices = new ArrayList<>();
-    double rate;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExpertGetCommentDTO {
+    String firstname;
+    String lastname;
+    String username;
     List<Comment> comments = new ArrayList<>();
-    boolean isDeleted;
 }
