234577f0cade8e9c93368bf664aeaeef4c2ba46d
reihaneh <reihaneh763@gmail.com>
2023-02-11 23:42:34 +0330

    add AddressMapper class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/AddressMapper.java
@@ -0,0 +1,18 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.AddressRequestDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response.AddressResponseDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Address;
+import org.mapstruct.factory.Mappers;
+
+public interface AddressMapper {
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    Address responseDtoToModel(AddressResponseDTO addressResponseDTO);
+
+    Address requestDtoToModel(AddressRequestDTO addressRequestDTO);
+
+    AddressResponseDTO modelToResponseDto(Address address);
+
+    AddressRequestDTO modelToRequestDto(Address address);
+}
