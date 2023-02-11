d1d546f40229da96bf5fa33c893794ad857e37a2
reihaneh <reihaneh763@gmail.com>
2023-02-12 01:15:10 +0330

    add ExpertUpdateDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ExpertUpdateDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/ExpertUpdateDTO.java
@@ -1,2 +1,27 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;public class ExpertUpdateDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.enums.ExpertStatus;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Comment;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Credit;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.SubService;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExpertUpdateDTO {
+    String firstname;
+    String lastname;
+    String emailAddress;
+    String username;
+    String password;
+    Credit credit;
+    ExpertStatus expertStatus;
+    byte[] personalPhoto;
 }
