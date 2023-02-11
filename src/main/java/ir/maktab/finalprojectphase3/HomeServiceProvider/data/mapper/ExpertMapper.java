04b1a156932c4f98ea19fdf2e9b2099de8807375
reihaneh <reihaneh763@gmail.com>
2023-02-12 01:11:19 +0330

    update ExpertMapper class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/ExpertMapper.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/ExpertMapper.java
@@ -1,10 +1,7 @@
 package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;
 
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.CommentRequestDTO;
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.ExpertRequestDTO;
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response.CommentResponseDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.*;
 import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response.ExpertResponseDTO;
-import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Comment;
 import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Expert;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -15,9 +12,16 @@ public interface ExpertMapper {
 
     Expert responseDtoToModel(ExpertResponseDTO expertResponseDTO);
 
-    Expert requestDtoToModel(ExpertRequestDTO expertRequestDTO);
+    Expert getCommentDtoToModel(ExpertGetCommentDTO expertGetCommentDTO);
 
-    ExpertResponseDTO modelToResponseDto(Expert baseService);
+    Expert registerDtoToModel(ExpertRegistrationDTO expertRegistrationDTO);
 
-    ExpertRequestDTO modelToRequestDto(Expert baseService);
+    Expert emailDtoToModel(ExpertEmailDTO expertEmailDTO);
+
+    Expert updateDtoToModel(ExpertUpdateDTO expertUpdateDTO);
+
+    Expert loginDtoToModel(LoginDTO loginDTO);
+    Expert changePasswordDtoToModel(ChangePasswordDTO changePasswordDTO);
+
+    ExpertResponseDTO modelToResponseDto(Expert expert);
 }
