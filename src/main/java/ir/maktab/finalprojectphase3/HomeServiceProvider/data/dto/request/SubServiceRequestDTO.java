db9abdffd795d5bd4cfad87f1f953b19377695a5
reihaneh <reihaneh763@gmail.com>
2023-02-11 22:08:37 +0330

    add SubServiceRequestDTO class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/request/SubServiceRequestDTO.java
@@ -0,0 +1,16 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SubServiceRequestDTO {
+    BaseServiceRequestDTO baseServiceRequestDTO;
+    String name;
+    String description;
+    Double basePrice;
+}
