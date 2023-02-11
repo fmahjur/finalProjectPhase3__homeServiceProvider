364532a15602181ca07592e49da75dc0bc5ded5c
reihaneh <reihaneh763@gmail.com>
2023-02-11 23:43:20 +0330

    add AddressResponseDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/AddressResponseDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/AddressResponseDTO.java
@@ -1,2 +1,15 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;public class AddressResponseDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.enums.City;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AddressResponseDTO {
+    City city;
+    String addressDetail;
 }
