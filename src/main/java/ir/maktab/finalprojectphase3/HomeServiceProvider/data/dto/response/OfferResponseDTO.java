26765e106f074785e50bec6206e933e8929a8f6e
reihaneh <reihaneh763@gmail.com>
2023-02-12 11:11:26 +0330

    add OfferResponseDTO class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/OfferResponseDTO.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/dto/response/OfferResponseDTO.java
@@ -1,2 +1,21 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;public class OfferResponseDTO {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Expert;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Orders;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OfferResponseDTO {
+    Expert expert;
+    Orders orders;
+    Long offerPrice;
+    Date proposedStartDate;
+    int durationOfWork;
 }
