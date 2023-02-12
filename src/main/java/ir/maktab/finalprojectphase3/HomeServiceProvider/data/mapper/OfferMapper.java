bfc23d31d5d6c866d1f6ef39996ea7c5f7a80d92
reihaneh <reihaneh763@gmail.com>
2023-02-12 11:11:43 +0330

    add OfferMapper class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/OfferMapper.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/OfferMapper.java
@@ -1,2 +1,20 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;public interface OfferMapper {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.OfferRequestDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response.OfferResponseDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Offer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OfferMapper {
+    OfferMapper INSTANCE = Mappers.getMapper(OfferMapper.class);
+
+    Offer responseDtoToModel(OfferResponseDTO baseServiceResponseDTO);
+
+    Offer requestDtoToModel(OfferRequestDTO baseServiceRequestDTO);
+
+    OfferResponseDTO modelToResponseDto(Offer offer);
+
+    OfferRequestDTO modelToRequestDto(Offer offer);
 }
