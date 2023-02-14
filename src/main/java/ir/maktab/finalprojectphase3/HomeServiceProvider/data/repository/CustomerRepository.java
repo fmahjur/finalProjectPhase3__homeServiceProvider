1473c04b2eaff43971b492b3bb67bf5959e1e053
reihaneh <reihaneh763@gmail.com>
2023-02-14 13:20:19 +0330

    add CustomerRepository class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/repository/CustomerRepository.java
@@ -0,0 +1,21 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.repository;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Customer> findByUsername(String email);
+
+    Optional<Customer> findByEmail(String emailAddress);
+    @Query("select c from Customer c where c.isDeleted= :isDeleted")
+    List<Customer> findAllByDeletedIs(boolean isDeleted);
+}
