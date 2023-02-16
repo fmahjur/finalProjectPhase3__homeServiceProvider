b8a695271cc33cc7e7afe3c87c9ca0ba91796e4b
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:25:30 +0330

    add InsufficientFoundsException class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/InsufficientFoundsException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;
+
+public class InsufficientFoundsException extends RuntimeException {
+    public InsufficientFoundsException(String message) {
+        super(message);
+    }
+}
