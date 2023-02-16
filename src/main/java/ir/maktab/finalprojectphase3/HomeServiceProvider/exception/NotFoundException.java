b23243a4c0a08ae9c8344057f674642fa37336cf
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:26:17 +0330

    add NotFoundException class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
