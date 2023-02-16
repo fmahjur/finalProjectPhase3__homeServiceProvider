0c9d09a79f431fe911bf070544d9b1b76204046c
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:34:52 +0330

    add OrderStatus enum

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.enums;
+
+public enum OrderStatus {
+    WAITING_FOR_EXPERTS_OFFER,
+    WAITING_FOR_CHOSE_EXPERT,
+    WAITING_FOR_EXPERT_TO_COME,
+    STARTED,
+    DONE,
+    PAID
+}
