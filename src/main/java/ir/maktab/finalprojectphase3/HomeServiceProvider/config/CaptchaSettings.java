7bb5ea987f782ac719797346696d61c2db0fd4cd
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:29:09 +0330

    add CaptchaSettings class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/config/CaptchaSettings.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/config/CaptchaSettings.java
@@ -1,2 +1,28 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.config;public class CaptchaSettings {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "google.recaptcha.key")
+public class CaptchaSettings {
+
+    private String site;
+    private String secret;
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
 }
