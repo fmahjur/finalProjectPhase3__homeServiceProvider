a9658ba29a269502b6d2b409e33de24f944cc070
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:25:09 +0330

    add IncorrectInformationException class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/IncorrectInformationException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;
+
+public class IncorrectInformationException extends RuntimeException {
+    public IncorrectInformationException(String message) {
+        super(message);
+    }
+}
