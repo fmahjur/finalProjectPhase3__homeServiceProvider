be545597c11a345ffbdb2500ab15f57e1e190cfa
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:36:22 +0330

    add OrderMapper class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/OrderMapper.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/data/mapper/OrderMapper.java
@@ -1,2 +1,25 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;public class OrderMapper {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.data.mapper;
+
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.OrderRequestDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.OrderUpdateDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.request.SubmitOrderDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.dto.response.OrderResponseDTO;
+import ir.maktab.finalprojectphase3.HomeServiceProvider.data.model.Orders;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Orders responseDtoToModel(OrderResponseDTO orderResponseDTO);
+
+    Orders requestDtoToModel(OrderRequestDTO orderRequestDTO);
+
+    Orders submitDtoToModel(SubmitOrderDTO submitOrderDTO);
+    Orders orderUpdateDtoToModel(OrderUpdateDTO orderUpdateDTO);
+
+    OrderResponseDTO modelToResponseDto(Orders orders);
+
+    OrderRequestDTO modelToRequestDto(Orders orders);
 }
