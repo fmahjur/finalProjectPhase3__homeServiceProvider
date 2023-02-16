1cfe00f4b74f577ff59b67e2f543a468f78237f8
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:28:03 +0330

    add ResourceNotFoundException class

---
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
