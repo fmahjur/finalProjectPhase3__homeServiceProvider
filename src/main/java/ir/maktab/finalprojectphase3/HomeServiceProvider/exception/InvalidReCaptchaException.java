da52ad6c1b4ecdf6c9020360985f480bf611bcf4
reihaneh <reihaneh763@gmail.com>
2023-02-16 16:25:53 +0330

    add InvalidReCaptchaException class

--- src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/InvalidReCaptchaException.java
+++ src/main/java/ir/maktab/finalprojectphase3/HomeServiceProvider/exception/InvalidReCaptchaException.java
@@ -1,2 +1,7 @@
-package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;public class InvalidReCaptchaException {
+package ir.maktab.finalprojectphase3.HomeServiceProvider.exception;
+
+public class InvalidReCaptchaException extends RuntimeException{
+    public InvalidReCaptchaException(String message) {
+        super(message);
+    }
 }
